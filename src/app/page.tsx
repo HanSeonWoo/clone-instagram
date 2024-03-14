c778beb55cb2e208ef9e478c3b97dca7aae64786
HanSeonWoo <118580135+HanSeonWoo@users.noreply.github.com>
2024-03-14 17:15:55 +0900

    로그인 페이지 구현 (#6)
    * react-icon, tailwind prettier 추가
    
    * favicon및 이미지 추가
    
    * 로그인 페이지

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Login from "@/components/Login";
 import Image from "next/image";
 
 export default function HomePage() {
   // session 유무에 따라 로그인 페이지를 노출할 수 있음.
-
+  if (true) {
+    return <Login />;
+  }
   return <main className="">인스타그램입니다</main>;
 }
